289964a01bcf68d0a31a3febd7a0070583770082
giimenez12 <tinchogimenez95@hotmail.com>
2021-05-09 04:22:55 -0300

    login required a todo

--- combi19app/views.py
+++ combi19app/views.py
@@ -102,6 +102,7 @@ class FormularioRegistro (HttpRequest):
             return render(request, "registrarse.html", {"mensaje": "not_ok", "errores": confirmacion})
 
 class FormularioRegistroChofer (HttpRequest):
+    @login_required
     def crear_formulario(request):
         registro = Registro()
         return render (request, "registrar_chofer.html", {"dato":registro})
@@ -116,12 +117,12 @@ class FormularioRegistroChofer (HttpRequest):
             confirmacion=errores(registro)
             registro = Registro()
             return render(request, "registrar_chofer.html", {"mensaje": "not_ok", "errores": confirmacion})
-
+    @login_required
     def editar_chofer(request, dni):
         chofer = Usuario.objects.get(dni=dni)
         registro = Registro(instance=chofer)
         return render(request, "modificar_chofer.html", {"dato": registro, "choferes": chofer})
-
+    @login_required
     def actualizar_chofer(request, dni):
         chofer = Usuario.objects.get(dni=dni)
         registro = Registro(request.POST, instance=chofer)
@@ -131,11 +132,11 @@ class FormularioRegistroChofer (HttpRequest):
         return render (request, "listar_choferes.html", {"choferes": choferes, "mensaje": "editado", "cantidad": len(choferes)})
 
 class FormularioVehiculo (HttpRequest):
-
+    @login_required
     def crear_formulario(request):
         vehiculo = Registro_vehiculo()
         return render (request, "agregar_vehiculo.html", {"dato": vehiculo})
-
+    @login_required
     def procesar_formulario(request):
         vehiculo = Registro_vehiculo(request.POST)
         if vehiculo.is_valid():
@@ -145,12 +146,12 @@ class FormularioVehiculo (HttpRequest):
         else:
             vehiculo = Registro_vehiculo()
             return render (request, "agregar_vehiculo.html", {"mensaje": "not_ok"})
-
+    @login_required
     def editar(request, patente_vehiculo):
         vehiculo = Vehiculo.objects.get(patente=patente_vehiculo)
         form = Registro_vehiculo(instance=vehiculo)
         return render (request, "modificar_vehiculo.html", {"form":form, "vehiculo":vehiculo})
-
+    @login_required
     def actualizar(request, patente_vehiculo):
         vehiculo = Vehiculo.objects.get(patente=patente_vehiculo)
         form = Registro_vehiculo(request.POST, instance = vehiculo)
@@ -161,22 +162,22 @@ class FormularioVehiculo (HttpRequest):
             return render (request, "listar_vehiculos.html", {"form":form,"vehiculos":vehiculos, "vehiculo":vehiculo, "mensaje": "editado"})
 
 class ListarVehiculos(HttpRequest):
-
+    @login_required
     def crear_listado(request):
         vehiculos = Vehiculo.objects.all()
         contexto = {'vehiculos': vehiculos, 'cantidad':len(vehiculos)}
         return render (request, "listar_vehiculos.html", contexto)
-
+    @login_required
     def mostrar_detalle(request, patente_vehiculo):
         detalle= Vehiculo.objects.get(pk=patente_vehiculo)
         return render (request, "listar_vehiculos.html", {"dato":detalle, "mensaje":"detalle"})
-
+    @login_required
     def mostrar_detalle_viaje_vehiculo(request, patente_vehiculo):
         detalle= Vehiculo.objects.get(pk=patente_vehiculo)
         return render (request, "listar_vehiculos.html", {"dato":detalle, "mensaje":"detalleViajeVehiculo"})
 
 class EliminarVehiculo(HttpRequest):
-
+    @login_required
     def eliminar_vehiculo(request, patente_vehiculo):
         vehiculo_eliminado = Vehiculo.objects.get(pk=patente_vehiculo)
         vehiculo_eliminado.delete()
@@ -184,7 +185,7 @@ class EliminarVehiculo(HttpRequest):
         return render (request, "listar_vehiculos.html", {"vehiculos": vehiculo, "mensaje":"eliminado", "cantidad": len(vehiculo)})
 
 class FormularioRuta (HttpRequest):
-
+    @login_required
     def crear_formulario(request):
         ruta = Registro_ruta()
         ciudad = Ciudad.objects.all()
@@ -203,13 +204,13 @@ class FormularioRuta (HttpRequest):
         confirmacion=errores_ruta(ruta)
         ruta = Registro_ruta()
         return render (request, "agregar_ruta.html", {"mensaje": "not_ok", "errores":confirmacion, "ciudades": ciudad})
-
+    @login_required
     def editar_ruta(request, nombre):
         ruta = Ruta.objects.get(nombre=nombre)
         registro = Registro_ruta(instance=ruta)
         ciudad = Ciudad.objects.all()
         return render(request, "modificar_ruta.html", {"dato": registro, "rutas": ruta, "ciudades": ciudad})
-
+    @login_required
     def actualizar_ruta(request, nombre):
         ruta = Ruta.objects.get(nombre=nombre)
         registro = Registro_ruta(request.POST, instance=ruta)
@@ -219,11 +220,11 @@ class FormularioRuta (HttpRequest):
         return render (request, "listar_rutas.html", {"rutas": rutas, "mensaje": "editado"})
 
 class FormularioCiudad (HttpRequest):
-
+    @login_required
     def crear_formulario(request):
         ciudad = Registro_ciudad()
         return render (request, "agregar_ciudad.html", {"dato": ciudad})
-
+    @login_required
     def procesar_formulario(request):
         ciudad = Registro_ciudad(request.POST)
         if ciudad.is_valid():
@@ -235,12 +236,12 @@ class FormularioCiudad (HttpRequest):
         confirmacion=errores_ciudad(ciudad)
         ciudad = Registro_ciudad()
         return render (request, "agregar_ciudad.html", {"mensaje": "not_ok", "errores":confirmacion})
-
+    @login_required
     def editar(request, codigo_postal):
         ciudad = Ciudad.objects.get(codigo_postal=codigo_postal)
         form = Registro_ciudad(instance=ciudad)
         return render (request, "modificar_ciudad.html", {"form":form, "ciudad":ciudad})
-
+    @login_required
     def actualizar(request, codigo_postal):
         ciudad = Ciudad.objects.get(codigo_postal=codigo_postal)
         form = Registro_ciudad(request.POST, instance = ciudad)
@@ -250,23 +251,23 @@ class FormularioCiudad (HttpRequest):
             return render (request, "listar_ciudades.html", {"form":form, "ciudad":ciudad, "mensaje": "ok", "ciudades":ciudades, "mensaje": "editado"})
 
 class ListarCiudad (HttpRequest):
-
+    @login_required
     def crear_listado(request):
         ciudad = Ciudad.objects.all()
         print(ciudad)
         contexto = {'ciudades': ciudad, 'cantidad':len(ciudad)}
         return render (request, "listar_ciudades.html", contexto)
-
+    @login_required
     def mostrar_detalle(request, codigo_postal):
         detalle= Ciudad.objects.get(pk=codigo_postal)
         return render (request, "listar_ciudades.html", {"dato":detalle, "mensaje":"detalle"})
-
+    @login_required
     def mostrar_detalle_viaje_ciudad(request, codigo_postal):
         detalle= Ciudad.objects.get(pk=codigo_postal)
         return render (request, "listar_ciudades.html", {"dato":detalle, "mensaje":"detalleViajeCiudad"})
 
 class EliminarCiudad (HttpRequest):
-
+    @login_required
     def eliminar_ciudad(request, codigo_postal):
         ciudad_eliminada = Ciudad.objects.get(pk=codigo_postal)
         ciudad_eliminada.delete()
@@ -274,20 +275,20 @@ class EliminarCiudad (HttpRequest):
         return render (request, "listar_ciudades.html", {"ciudades": ciudad, "mensaje":"eliminado", "cantidad": len(ciudad)})
 
 class ListarRuta (HttpRequest):
-
+    @login_required
     def crear_listado(request):
         ruta = Ruta.objects.all()
         contexto = {'rutas': ruta, 'cantidad':len(ruta)}
         return render (request, "listar_rutas.html", contexto)
-
+    @login_required
     def mostrar_detalle(request, nombre):
         detalle= Ruta.objects.get(pk=nombre)
         return render (request, "listar_rutas.html", {"dato":detalle, "mensaje":"detalle"})
-
+    @login_required
     def mostrar_detalle_viaje_ruta(request, nombre):
         detalle= Ruta.objects.get(pk=nombre)
         return render (request, "listar_rutas.html", {"dato":detalle, "mensaje":"detalleViajeRuta"})
-
+    @login_required
     def eliminar_ruta(request, nombre):
         ruta_eliminada = Ruta.objects.get(pk=nombre)
         ruta_eliminada.delete()
@@ -306,7 +307,7 @@ def errores_viajes(viaje, dato):
     return viaje
 
 class FormularioViaje (HttpRequest):
-
+    @login_required
     def crear_formulario(request):
         viaje = Registro_viaje()
         minutos = calcular_minutos()
@@ -315,7 +316,7 @@ class FormularioViaje (HttpRequest):
         ciudades = Ciudad.objects.all()
         rutas = Ruta.objects.all()
         return render (request, "agregar_viaje.html", {"dato": viaje, "minutos":minutos, "choferes":choferes, "vehiculos": vehiculos, "ciudades":ciudades, "rutas":rutas})
-
+    @login_required
     def procesar_formulario(request):
         viaje = Registro_viaje(request.POST)
         minutos = calcular_minutos()
@@ -333,7 +334,7 @@ class FormularioViaje (HttpRequest):
         confirmacion=errores_viaje(viaje)
         viaje = Registro_viaje()
         return render (request, "agregar_viaje.html", {"errores":confirmacion, "mensaje": "not_ok", "minutos":minutos, "choferes":choferes, "vehiculos": vehiculos, "ciudades":ciudades, "rutas":rutas})
-
+    @login_required
     def editar_viaje(request, id_viaje):
         viaje = Viaje.objects.get(id=id_viaje)
         registro = Registro_viaje(instance=viaje)
@@ -401,12 +402,12 @@ class FormularioViaje (HttpRequest):
         return render (request, "listar_viajes.html", {"viajes": viajes, "mensaje": "editado", "cantidad": len(viajes)})
 
 class ListarViajes(HttpRequest):
-
+    @login_required
     def crear_listado(request):
         viaje = Viaje.objects.all()
         contexto = {'viajes': viaje, 'cantidad':len(viaje)}
         return render (request, "listar_viajes.html", contexto)
-
+    @login_required
     def mostrar_detalle(request, id_viaje):
         detalle= Viaje.objects.get(id=id_viaje)
         return render (request, "listar_viajes.html", {"dato":detalle, "mensaje":"detalle"})
@@ -420,15 +421,15 @@ class ListarChofer(HttpRequest):
         chofer = Usuario.objects.filter(tipo_usuario=2)
         contexto = {'choferes': chofer, 'cantidad': len(chofer)}
         return render (request, "listar_choferes.html", contexto)
-
+    @login_required
     def mostrar_detalle(request, dni):
         detalle = Usuario.objects.get(pk=dni)
         return render (request, "listar_choferes.html", {"dato": detalle, "mensaje":"detalle"})
-
+    @login_required
     def mostrar_detalle_viaje_chofer(request, dni):
         detalle = Usuario.objects.get(pk=dni)
         return render (request, "listar_choferes.html", {"dato": detalle, "mensaje":"detalleViajeChofer"})
-
+    @login_required
     def eliminar_chofer(request, dni):
         chofer_eliminado = Usuario.objects.get(pk=dni)
         chofer_eliminado.delete()
